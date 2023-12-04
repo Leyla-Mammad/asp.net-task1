d40eb1474fa926f572b409412155e00279c62b0d
Leyla-Mammad <leylamamedova19960@gmail.com>
2023-12-04 04:35:31 +0400

    asp.net task1

---
+++ asp.net task1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp.net_task1.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+
+            ViewBag.Name = "Leyla";
+            ViewBag.Surname = "Mammadova";
+            ViewBag.Age = 27;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "+99455 555 55 55";
+            return View();
+        }
+    }
+}
